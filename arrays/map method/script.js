fbaf6709cb6685936675d9cb6f0fee12733e499d
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-03-24 04:44:14 +0500

    concepts of map method

---
+++ arrays/map method/script.js
@@ -0,0 +1,22 @@
+let todos = [
+    {id:1,text:"first task is coding",toggle: false},
+    {id:2,text:"Second task is execrising",toggle: false},
+    {id:3,text:"Third task is execrising",toggle: false},
+     
+
+]
+
+const updateToDo=(ID,todo)=>{
+  const newarray = (todos.map((prevobj)=>prevobj.id === ID ? todo: prevobj))
+  console.log('before:',todos)
+  todos = newarray;
+  
+  console.log('after',newarray)
+
+
+
+}
+
+updateToDo(2, {id:2,text:"New object is inserted",toggle: true})
+
+
