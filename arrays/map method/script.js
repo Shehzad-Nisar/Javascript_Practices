22a81acecc0e47fd9fc5b850ae7130d3448b8cc9
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-03-25 09:18:11 +0500

    new changes

--- arrays/map method/script.js
+++ arrays/map method/script.js
@@ -18,9 +18,21 @@ console.log(var1)
 //deletedTodo function:
 
 const deletedTodo=(id)=>{
-  return todos.filter((eachTodo)=>eachTodo.id!=id?eachTodo:'')
+  return todos.filter((eachTodo)=>eachTodo.id!=id)
 }
 
-const var2= deletedTodo(1);
+const var2= deletedTodo(3);
 console.log("after deleted",var2);
 
+
+//completed function:
+
+const completedTodo=(id)=>{
+  return todos.map((eachTodo)=> eachTodo.id===id?{...eachTodo, completed: !eachTodo.completed}: eachTodo)
+
+
+
+}
+
+const var3 = completedTodo(1);
+console.log(var3);
